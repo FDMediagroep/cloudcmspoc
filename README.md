d870686c0a1d9bf332018ede1757f2302f2e7f7c
Alexander Eerenberg <alexander.eerenberg@fdmediagroep.nl>
2020-11-16 14:03:51 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cloudcmspoc
+You can open the index.html file or run `npm run serve` after you've done a `npm install`. The latter might be overkill for now hehe.
---
+++ ckeditor/ckeditor4/plugins/poc_plugin/plugin.js
@@ -0,0 +1,18 @@
+CKEDITOR.plugins.add("poc_plugin", {
+  icons: "timestamp",
+  init: function (editor) {
+    editor.addCommand("insertTimestamp", {
+      exec: function (editor) {
+        var now = new Date();
+        editor.insertHtml(
+          "The current date and time is: <em>" + now.toString() + "</em>"
+        );
+      },
+    });
+    editor.ui.addButton("Timestamp", {
+      label: "Insert Timestamp",
+      command: "insertTimestamp",
+      toolbar: "insert",
+    });
+  },
+});
